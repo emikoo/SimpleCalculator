35bf52455bdcebb867fef74662abf989015dc93f
emikoo <emikoo@users.noreply.github.com>
2020-10-17 20:22:13 +0600

    hw003 isEmpty() isBlank()

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -5,53 +5,71 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var isMinus = false
+    var textArray = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getTotal()
-        setPlusOperator()
-        setMinusOperator()
+        addToArray()
+        removeFromArray()
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun getTotal() {
-        btnGo.setOnClickListener {
-            val firstNum: Int = edFirstNum.text.toString().toInt()
-            val secondNum: Int = edSecondNum.text.toString().toInt()
-            var result: Int? = null
-            if(isMinus) result = firstNum - secondNum
-            else result = firstNum + secondNum
+    //textChangedListener
 
-            tvTotal.text = "= $result"
-            tvPlusOrMinus.text = "__"
-            edFirstNum.text.clear()
-            edSecondNum.text.clear()
+    private fun addToArray(){
+        btnAdd.setOnClickListener {
+            val value = edText.text.toString()
+            if (value.isBlank() || value.isEmpty()) {
+                edText.text.clear()
+            } else {
+                edText.text.clear()
+                textArray.add(value)
+                displayArray()
+            }
         }
     }
 
-    private fun setPlusOperator(){
-        btnPlus.setOnClickListener {
-            isMinus = false
-
-            tvTotal.text = ""
-            tvPlusOrMinus.text = "+"
+    private fun removeFromArray(){
+        btnRemove.setOnClickListener {
+            var value = edText.text.toString()
+            if (value.isBlank() || value.isEmpty()) {
+                edText.text.clear()
+            } else {
+                edText.text.clear()
+                findAndRemoveFromArray(value)
+                displayArray()
+            }
         }
     }
 
-    private fun setMinusOperator(){
-        btnMinus.setOnClickListener {
-            isMinus = true
-
-            tvTotal.text = ""
-            tvPlusOrMinus.text = "-"
+    private fun findAndRemoveFromArray(value: String) {
+        var indexOfArray: Int? = null
+        var elements = ""
+        for((i,text) in textArray.withIndex()){
+            if (text == value) {
+                indexOfArray = i
+                elements = text
+            }
         }
+        if(indexOfArray != null){
+            textArray.removeAt(indexOfArray)
+            Toast.makeText(this, "$elements удален", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun displayArray() {
+        var result = ""
+        for(list in textArray){
+            result = "$result $list \n"
+        }
+        tvList.text = result
     }
 }
