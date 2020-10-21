892cbd22dd6f6f116aed42ca04e227c324e685af
emikoo <emikoo@users.noreply.github.com>
2020-10-21 21:17:51 +0600

    hw004

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -14,11 +14,22 @@ import kotlinx.android.synthetic.main.constraint_layout.*
 
 class MainActivity : AppCompatActivity() {
 
+    var username = "Адеми"
+    var lastname = "Абдыкеримова"
+    var age = "16"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout)
 
+        setupView()
         saveInfoInET()
+    }
+
+    private fun setupView(){
+        tvUsername.text = username
+        tvLastName.text = lastname
+        tvAge.text = age
     }
 
     private fun saveInfoInET(){
@@ -27,26 +38,23 @@ class MainActivity : AppCompatActivity() {
             val cityName = edCityName.text.toString()
             val regionName = edRegionName.text.toString()
 
-            if(phoneNumber.isEmpty()){
-                showToast("Поле номер телефона не может быть пустым")
-                clearET()
-            } else if(cityName.isEmpty()){
-                showToast("Поле города не может быть пустым")
-                clearET()
-            } else if(regionName.isEmpty()){
-                showToast("Поле область не может быть пустым")
-                clearET()
-            } else {
-                val allInformation = "$phoneNumber $cityName $regionName \n"
-                Log.v("ACTIVITY_INFORMATION", allInformation)
+            if (phoneNumber.isEmpty() && cityName.isEmpty() && regionName.isEmpty()){
+                showToast("Заполните все поля")
+                return@setOnClickListener
             }
+            checkFields(phoneNumber, "Поле номер телефона не может быть пустым")
+            checkFields(cityName, "Поле города не может быть пустым")
+            checkFields(regionName, "Поле область не может быть пустым")
+            val allInformation = "$phoneNumber, $cityName, $regionName"
+            Log.v("ACTIVITY_INFORMATION", allInformation)
         }
     }
-
-    private fun clearET(){
-        edPhoneNumber.text.clear()
-        edCityName.text.clear()
-        edRegionName.text.clear()
+    
+    private fun checkFields(value: String, message: String) {
+        if (value.isEmpty() || value.isBlank()) {
+            showToast(message)
+            return
+        }
     }
 
     private fun showToast(message: String) {
