0c82821210e1044809e11d52d68a758861496ebd
emikoo <emikoo@users.noreply.github.com>
2020-10-22 22:00:33 +0600

    hw005 (without descriptions of cities)

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firsthomework
 
 import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -11,53 +12,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.constraint_layout.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    var username = "Адеми"
-    var lastname = "Абдыкеримова"
-    var age = "16"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout)
 
-        setupView()
-        saveInfoInET()
+        changeIconAction()
     }
 
-    private fun setupView(){
-        tvUsername.text = username
-        tvLastName.text = lastname
-        tvAge.text = age
-    }
-
-    private fun saveInfoInET(){
-        btnSave.setOnClickListener {
-            val phoneNumber = edPhoneNumber.text.toString()
-            val cityName = edCityName.text.toString()
-            val regionName = edRegionName.text.toString()
-
-            if (phoneNumber.isEmpty() && cityName.isEmpty() && regionName.isEmpty()){
-                showToast("Заполните все поля")
-                return@setOnClickListener
-            }
-            checkFields(phoneNumber, "Поле номер телефона не может быть пустым")
-            checkFields(cityName, "Поле города не может быть пустым")
-            checkFields(regionName, "Поле область не может быть пустым")
-            val allInformation = "$phoneNumber, $cityName, $regionName"
-            Log.v("ACTIVITY_INFORMATION", allInformation)
+    private fun changeIconAction() {
+        refresh_image_button.setOnClickListener {
+            val citiesArray = arrayOf(R.mipmap.ic_tokyo, R.mipmap.ic_fukuoka, R.mipmap.ic_kyoto,
+                R.mipmap.ic_nagoya, R.mipmap.ic_osaka, R.mipmap.ic_sapporo, R.mipmap.ic_yokohama)
+            .random()
+            image.setImageResource(citiesArray)
         }
-    }
-    
-    private fun checkFields(value: String, message: String) {
-        if (value.isEmpty() || value.isBlank()) {
-            showToast(message)
-            return
-        }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
