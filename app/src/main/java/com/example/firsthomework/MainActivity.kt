2a6d5c0efceb396e7465d25432c5ceee94bb8a2a
emikoo <emikoo@users.noreply.github.com>
2020-10-22 23:28:45 +0600

    hw005

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -16,6 +16,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val citiesArray = arrayOf(R.mipmap.ic_tokyo, R.mipmap.ic_fukuoka, R.mipmap.ic_kyoto,
+        R.mipmap.ic_nagoya, R.mipmap.ic_osaka, R.mipmap.ic_sapporo, R.mipmap.ic_yokohama)
+
+    var indexOfArray: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout)
@@ -25,10 +30,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeIconAction() {
         refresh_image_button.setOnClickListener {
-            val citiesArray = arrayOf(R.mipmap.ic_tokyo, R.mipmap.ic_fukuoka, R.mipmap.ic_kyoto,
-                R.mipmap.ic_nagoya, R.mipmap.ic_osaka, R.mipmap.ic_sapporo, R.mipmap.ic_yokohama)
-            .random()
-            image.setImageResource(citiesArray)
+            val citiesRandom = citiesArray.random()
+            image.setImageResource(citiesRandom)
+
+            for((index, value) in citiesArray.withIndex()) {
+                if (value == citiesRandom){
+                    indexOfArray = index
+                }
+            }
+            showDescription(0, "Токио", "Токио — столица и крупнейший мегаполис страны")
+            showDescription(1,"Фукуока", "Крупнейшим мегаполисом на острове Кюсю является " +
+                        "мегаполис Фукуока. Фукуока — это индустриальный город с развитой " +
+                        "сферой услуг, логистический центр.")
+            showDescription(2,"Киото", "Киото — древняя столица страны. В каждом районе города " +
+                        "расположены многочисленные буддийские храмы и сионистские святилища.")
+            showDescription(3,"Нагоя", "Еще один мегаполис страны — Нагоя. В городе развита " +
+                        "металлургия, машиностроение, нефтехимическая промышленность.")
+            showDescription(4,"Осака", "Осака находится на острове Хонсю и отличается портовым " +
+                        "статусом. Тут развит культ еды.")
+            showDescription(5,"Саппоро", "Крупнейшим японским городом острова Хоккайдо является " +
+                        "Саппоро. Благодаря мягкому климату, Саппоро был выбран столицей " +
+                        "Олимпиады зимних игр. Здесь находится один из лучших в мире центр для " +
+                        "занятий горнолыжным спортом.")
+            showDescription(6,"Йокогама", "Йокогама — второй по численности и важности мегаполис" +
+                        " Японии. Сегодня это мощный экономический и промышленный центр.")
+        }
+    }
+
+    private fun showDescription(i: Int, name: String, value: String){
+        if(indexOfArray == i) {
+            city_name_text_view.text = name
+            description_text_view.text = value
+            return
         }
     }
 }
