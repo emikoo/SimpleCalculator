c7bee35df08ab34d99eed6557e67a5e84eaca7f0
emikoo <emikoo@users.noreply.github.com>
2020-10-24 21:04:42 +0600

    hw005 optimize(with crash))

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.example.firsthomework
 
-import android.annotation.SuppressLint
-import android.media.Image
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.firsthomework.models.City
 import kotlinx.android.synthetic.main.constraint_layout.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val citiesArray = arrayOf(R.mipmap.ic_tokyo, R.mipmap.ic_fukuoka, R.mipmap.ic_kyoto,
-        R.mipmap.ic_nagoya, R.mipmap.ic_osaka, R.mipmap.ic_sapporo, R.mipmap.ic_yokohama)
-
+    var descriptionArray: List<City>? = null
     var indexOfArray: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,35 +22,73 @@ class MainActivity : AppCompatActivity() {
             val citiesRandom = citiesArray.random()
             image.setImageResource(citiesRandom)
 
-            for((index, value) in citiesArray.withIndex()) {
-                if (value == citiesRandom){
-                    indexOfArray = index
-                }
-            }
-            showDescription(0, "Токио", "Токио — столица и крупнейший мегаполис страны")
-            showDescription(1,"Фукуока", "Крупнейшим мегаполисом на острове Кюсю является " +
-                        "мегаполис Фукуока. Фукуока — это индустриальный город с развитой " +
-                        "сферой услуг, логистический центр.")
-            showDescription(2,"Киото", "Киото — древняя столица страны. В каждом районе города " +
-                        "расположены многочисленные буддийские храмы и сионистские святилища.")
-            showDescription(3,"Нагоя", "Еще один мегаполис страны — Нагоя. В городе развита " +
-                        "металлургия, машиностроение, нефтехимическая промышленность.")
-            showDescription(4,"Осака", "Осака находится на острове Хонсю и отличается портовым " +
-                        "статусом. Тут развит культ еды.")
-            showDescription(5,"Саппоро", "Крупнейшим японским городом острова Хоккайдо является " +
-                        "Саппоро. Благодаря мягкому климату, Саппоро был выбран столицей " +
-                        "Олимпиады зимних игр. Здесь находится один из лучших в мире центр для " +
-                        "занятий горнолыжным спортом.")
-            showDescription(6,"Йокогама", "Йокогама — второй по численности и важности мегаполис" +
-                        " Японии. Сегодня это мощный экономический и промышленный центр.")
+            getIndexFromElement(citiesRandom)
         }
     }
 
-    private fun showDescription(i: Int, name: String, value: String){
-        if(indexOfArray == i) {
-            city_name_text_view.text = name
-            description_text_view.text = value
-            return
+    private fun getIndexFromElement(element: Int) {
+        cityArray()
+        for ((index, value) in citiesArray.withIndex()) {
+            if (value == element) indexOfArray = index
         }
+        getDescriptionToImage(0)
+        getDescriptionToImage(1)
+        getDescriptionToImage(2)
+        getDescriptionToImage(3)
+        getDescriptionToImage(4)
+        getDescriptionToImage(5)
+        getDescriptionToImage(6)
+
+        return
+    }
+
+    private fun getDescriptionToImage(index: Int){
+        if (indexOfArray == index) {
+            title_text_view.text = descriptionArray?.get(index)?.name
+            description_text_view.text = descriptionArray?.get(index)?.description
+        }
+        return
+    }
+
+    private val citiesArray = mutableListOf<Int>().apply {
+        R.mipmap.ic_tokyo
+        R.mipmap.ic_fukuoka
+        R.mipmap.ic_kyoto
+        R.mipmap.ic_nagoya
+        R.mipmap.ic_osaka
+        R.mipmap.ic_sapporo
+        R.mipmap.ic_yokohama
+    }
+
+    private fun cityArray() {
+        descriptionArray = listOf(
+            City(0, "Токио", "Токио — столица и крупнейший мегаполис страны"),
+            City(
+                1, "Фукуока", "Крупнейшим мегаполисом на острове Кюсю является " +
+                        "мегаполис Фукуока. Фукуока — это индустриальный город с развитой сферой услуг, логистический центр."
+            ),
+            City(
+                2, "Киото", "Киото — древняя столица страны. В каждом районе города " +
+                        "расположены многочисленные буддийские храмы и сионистские святилища."
+            ),
+            City(
+                3, "Нагоя", "Еще один мегаполис страны — Нагоя. В городе развита " +
+                        "металлургия, машиностроение, нефтехимическая промышленность."
+            ),
+            City(
+                4,
+                "Осака",
+                "Осака находится на острове Хонсю и отличается портовым статусом. Тут развит культ еды."
+            ),
+            City(
+                5, "Саппоро", "Крупнейшим японским городом острова Хоккайдо является " +
+                        "Саппоро. Благодаря мягкому климату, Саппоро был выбран столицей Олимпиады зимних игр. Здесь " +
+                        "находится один из лучших в мире центр для занятий горнолыжным спортом."
+            ),
+            City(
+                6, "Йокогама", "Йокогама — второй по численности и важности мегаполис Японии. " +
+                        "Сегодня это мощный экономический и промышленный центр."
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/firsthomework/models/City.kt
@@ -0,0 +1,7 @@
+package com.example.firsthomework.models
+
+data class City(
+    val index: Int,
+    val name: String,
+    val description: String
+)
