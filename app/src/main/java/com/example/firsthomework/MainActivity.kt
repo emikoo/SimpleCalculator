604e09166f8f726c08b0f4ad36dd5739e4802cd5
emikoo <emikoo@users.noreply.github.com>
2020-10-25 21:55:19 +0600

    hw006

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -1,93 +1,65 @@
 package com.example.firsthomework
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.firsthomework.models.City
-import kotlinx.android.synthetic.main.constraint_layout.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //Каждый раз когда вы нажимаете на кнопку перейти на 2-й активити, то добавлять ваше значение в Array
+    //Добавить вторую кнопку в mainActivity, при нажатии на которую вы будете выводить все значения в Toast
 
-    private var descriptionArray: List<City>? = null
-    private var indexOfArray: Int? = null
-
-    private var citiesArray = arrayOf(
-        R.mipmap.ic_tokyo,
-        R.mipmap.ic_fukuoka,
-        R.mipmap.ic_kyoto,
-        R.mipmap.ic_nagoya,
-        R.mipmap.ic_osaka,
-        R.mipmap.ic_sapporo,
-        R.mipmap.ic_yokohama
-    )
-
-    private fun cityArray() {
-        descriptionArray = listOf(
-            City(0, "Токио", "Токио — столица и крупнейший мегаполис страны"),
-            City(
-                1, "Фукуока", "Крупнейшим мегаполисом на острове Кюсю является " +
-                        "мегаполис Фукуока. Фукуока — это индустриальный город с развитой сферой услуг, логистический центр."
-            ),
-            City(
-                2, "Киото", "Киото — древняя столица страны. В каждом районе города " +
-                        "расположены многочисленные буддийские храмы и сионистские святилища."
-            ),
-            City(
-                3, "Нагоя", "Еще один мегаполис страны — Нагоя. В городе развита " +
-                        "металлургия, машиностроение, нефтехимическая промышленность."
-            ),
-            City(
-                4,
-                "Осака",
-                "Осака находится на острове Хонсю и отличается портовым статусом. Тут развит культ еды."
-            ),
-            City(
-                5, "Саппоро", "Крупнейшим японским городом острова Хоккайдо является " +
-                        "Саппоро. Благодаря мягкому климату, Саппоро был выбран столицей Олимпиады зимних игр. Здесь " +
-                        "находится один из лучших в мире центр для занятий горнолыжным спортом."
-            ),
-            City(
-                6, "Йокогама", "Йокогама — второй по численности и важности мегаполис Японии. " +
-                        "Сегодня это мощный экономический и промышленный центр."
-            )
-        )
-    }
+    var text: String? = null
+    var array = mutableListOf<String>()
+    var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.constraint_layout)
+        setContentView(R.layout.activity_main)
 
-        cityArray()
-        changeIconAction()
+        getValueAction()
+        showArrayAction()
     }
 
-    private fun changeIconAction() {
-        refresh_image_button.setOnClickListener {
-            val citiesRandom = citiesArray.random()
-            image.setImageResource(citiesRandom)
+    private fun getValueAction() {
+        btnShow.setOnClickListener {
+            text = edText.text.toString()
 
-            getIndexFromElement(citiesRandom)
+            if (text!!.isEmpty() || text!!.isBlank()) {
+                showToast("Поле не дожно быть пустым")
+                edText.text.clear()
+            } else {
+                array.add("$text")
+
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("text", text)
+                edText.text.clear()
+
+                startActivity(intent)
+            }
         }
     }
 
-    private fun getIndexFromElement(element: Int) {
-        for ((index, value) in citiesArray.withIndex()) {
-            if (value == element) indexOfArray = index
-            getDescriptionToImage(0)
-            getDescriptionToImage(1)
-            getDescriptionToImage(2)
-            getDescriptionToImage(3)
-            getDescriptionToImage(4)
-            getDescriptionToImage(5)
-            getDescriptionToImage(6)
+    private fun showArrayAction(){
+        btnShowAllValues.setOnClickListener {
+                getArray()
         }
-        return
     }
 
-    private fun getDescriptionToImage(index: Int){
-        if (indexOfArray == index) {
-            city_name_text_view.text = descriptionArray?.get(index)?.name
-            description_text_view.text = descriptionArray?.get(index)?.description
+    private fun getArray() {
+        var list = ""
+        for (listArray in array) {
+            list = "$list $listArray \n"
         }
-        return
+        showToast(list)
+    }
+
+    @SuppressLint("ShowToast")
+    private fun showToast(message: String) {
+        if (toast != null) toast?.cancel()
+        toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
+        toast?.show()
     }
 }
--- app/src/main/java/com/example/firsthomework/models/City.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firsthomework.models
-
-data class City(
-    val index: Int,
-    val name: String,
-    val description: String
-)
