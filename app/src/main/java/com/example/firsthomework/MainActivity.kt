d12435fb943bda91fd6b29c2ce863858a9a64b5e
emikoo <emikoo@users.noreply.github.com>
2020-10-30 17:16:13 +0600

    hw008 (without double optimization)

--- app/src/main/java/com/example/firsthomework/MainActivity.kt
+++ app/src/main/java/com/example/firsthomework/MainActivity.kt
@@ -7,52 +7,120 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var enteredNumber = 0
+    private var operand = ""
+    private var default = ""
+    private var lastNumber = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showSymbol()
-        getResetAction()
+        btnDelete()
+        btnReset()
+        btnEquals()
     }
 
     private fun showSymbol(){
-        btnSymbol("1", btn1)
-        btnSymbol("2", btn2)
-        btnSymbol("3", btn3)
-        btnSymbol("4", btn4)
-        btnSymbol("5", btn5)
-        btnSymbol("6", btn6)
-        btnSymbol("7", btn7)
-        btnSymbol("8", btn8)
-        btnSymbol("9", btn9)
+        btnDecimal("0", btn0)
+        btnDecimal("1", btn1)
+        btnDecimal("2", btn2)
+        btnDecimal("3", btn3)
+        btnDecimal("4", btn4)
+        btnDecimal("5", btn5)
+        btnDecimal("6", btn6)
+        btnDecimal("7", btn7)
+        btnDecimal("8", btn8)
+        btnDecimal("9", btn9)
 
-        btnSymbol("÷", btnShare)
-        btnSymbol("×", btnPow)
-        btnSymbol("–", btnMinus)
-        btnSymbol("+", btnPlus)
+        getOperandAction("+", btnPlus)
+        getOperandAction("–", btnMinus)
+        getOperandAction("×", btnPow)
+        getOperandAction("÷", btnSplit)
     }
 
-    private fun getDeleteLastSymbolAction({
-        btnDelete.setOnClickListener {
-
-        }
-    })
-
-    private fun getResetAction(){
-        btnReset.setOnClickListener {
-            tvProcess.text = ""
-        }
-    }
-
-    private fun btnSymbol(symbol: String, btn: Button) {
+    private fun btnDecimal(number: String, btn: Button) {
         btn.setOnClickListener {
-            var default = tvProcess.text.toString()
-            default += "$symbol"
+            default = tvProcess.text.toString()
+            default += "$number"
+            lastNumber += "$number"
             tvProcess.text = default
         }
     }
 
+    private fun getOperandAction(operando: String, btn: Button) {
+        btn.setOnClickListener {
+            operand = operando
+            default = tvProcess.text.toString()
+            if (isDecimal(default.last())) {
+                if (enteredNumber == 0) enteredNumber += lastNumber.toInt()
+                else enteredNumber *= lastNumber.toInt()
+                lastNumber = ""
+                default += "$operando"
+                tvProcess.text = default
+            } else {
+                default = default.dropLast(1)
+                default += "$operando"
+                tvProcess.text = default
+            }
+        }
+    }
+
+    private fun btnDelete(){
+        btnDelete.setOnClickListener{
+            default = tvProcess.text.toString()
+            default = default.dropLast(1)
+            tvProcess.text = default
+        }
+    }
+
+    private fun btnReset() {
+        btnReset.setOnClickListener {
+            enteredNumber = 0
+            lastNumber = ""
+            tvProcess.text = ""
+            tvResult.text = ""
+        }
+    }
+
+    private fun btnEquals() {
+        btnEquals.setOnClickListener {
+            when (operand) {
+                "×" -> {
+                    val sum = enteredNumber * lastNumber.toInt()
+                    tvProcess.text = ""
+                    tvResult.text = sum.toString()
+                }
+                "÷" -> {
+                    val sum = enteredNumber / lastNumber.toDouble()
+                    tvProcess.text = ""
+                    tvResult.text = sum.toString()
+                }
+                "+" -> {
+                    val sum = enteredNumber + lastNumber.toInt()
+                    tvProcess.text = ""
+                    tvResult.text = sum.toString()
+                }
+                "–" -> {
+                    val sum = enteredNumber - lastNumber.toInt()
+                    tvProcess.text = ""
+                    tvResult.text = sum.toString()
+                }
+            }
+        }
+    }
+
+    private fun isDecimal(last: Char): Boolean {
+        val charArray = mutableListOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+        for (char in charArray) {
+            if (last == char) return true
+        }
+        return false
+    }
 }
